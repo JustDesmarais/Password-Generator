1e1b4a802e7884724cff40cb683f6ff5991b57e2
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-09-25 10:04:52 -0500

    variables added

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// global variables
+const letters = "abcdefghijklmnopqrstuvwxyz"; // letters available to for password
+var lettersArray = letters.split(''); // letters as an array
+
+const specChar = ' !"#$%&' + "()*+,-./:;<=>?@[\]^_`{|}~"; // List of special characters.  Concatenated to inlude single and double quotation marks
+var specCharArray = specChar.split(''); // specChar as an array
+
+var passLength = 8; // variable for length of characters of password
+
+var lower = true; // boolean to include lowercase characters in password
+var upper = true; // boolean to include upperchase characters in password
+var digits = true; // boolean to include numbers in password
+var specCharOpt = true; // boolean to include special characters in password
+
+console.log(specCharArray);
+console.log(lettersArray);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -6,6 +23,7 @@ document.getElementById("generate").addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
