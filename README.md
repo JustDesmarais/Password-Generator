9905c53a467f8b21f335ddf890cbd6e0570073ca
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-09-25 22:57:58 -0500

    update randomizer function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Password-Generator
+
 Application that enables employees to generate random passwords based on criteria that they’ve selected.
+
+Starter code (html, css, and js) provided by Bootcampspot.
+
+for loop adapted from Melvin Adekanye, @MelvinAdekanye, on youtube.com
+
+https://www.youtube.com/watch?v=jww3V2fSQyg
+
--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 // global variables
 const letters = "abcdefghijklmnopqrstuvwxyz"; // letters available to for password
-var lettersArray = letters.split(''); // letters as an array
-
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var upperLettersArray = upperLetters.split('');
-
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const specChar = ' !"#$%&' + "()*+,-./:;<=>?@[\]^_`{|}~"; // List of special characters.  Concatenated to inlude single and double quotation marks
-var specCharArray = specChar.split(''); // specChar as an array
-
 const digits = "0123456789"
-var digitsArray = digits.split('');
 
 var passLength = null; // variable for length of characters of password
 
@@ -20,49 +13,11 @@ var specCharOpt = true; // boolean to include special characters in password
 var review = false; // boolean to allow user to review criteria options
 
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var passRun = "";
-  var charSet = []; 
-  if (lower === true) {
-    charSet.push(lettersArray);
-  }
-  if (upper === true) {
-    charSet.push(upperLettersArray);
-  }
-  if (digitsOpt === true) {
-    charSet.push(digitsArray);
-  }
-  if (specCharOpt === true) {
-    charSet.push(specCharArray);
-  }
-  for (var i, i = 0; i < charSet.length; i++) {
-    var charString = "" + charSet[i];
-    console.log(charString);
-  }
-  for (var i, i = 0; i < passLength; i++) {
-    passRun += charString.charAt(Math.floor(Math.random() * charString.length))
-  }
-  if (lower === true && passRun.includes(letters) === false) {
-    generatePassword();
-  }
-  if (upper === true && passRun.includes(letters.toUpperCase) === false) {
-    generatePassword();
-  }
-  if (digitsOpt === true && passRun.includes(digits) === false) {
-    generatePassword();
-  }
-  if (specCharOpt === true && passRun.includes(specChar) === false) {
-    generatePassword();
-  }
-  console.log(passRun);
-}
+generateBtn.addEventListener("click", writePassword); // Event listener to generate button
 
-
-
-// Write password to the #password input
+// user selections for password criteria
 function writePassword() {
   passLength = window.prompt("Set number of characters between 8 and 128");
   if (passLength === null) {
@@ -81,7 +36,7 @@ function writePassword() {
 
     digitsOpt = window.confirm("Do you want your password to include numbers?");
     specCharOpt = window.confirm("Do you want your password to include special characters?");
-    review = window.confirm("Does this look correct?\nCharacter Length: " + passLength + "\nLowercase Characters:" + lower + "\nUPPERCASE Characters: " + upper + "\nInclude Numbers: " + digitsOpt + "\nSpecial Characters: " + specCharOpt);
+    review = window.confirm("Does this look correct?\nCharacter Length: " + passLength + "\nLowercase Characters: " + lower + "\nUPPERCASE Characters: " + upper + "\nInclude Numbers: " + digitsOpt + "\nSpecial Characters: " + specCharOpt);
     
     if (review === true) {
       generatePassword();
@@ -89,39 +44,32 @@ function writePassword() {
       writePassword();
     }
   }
+};
 
-  var password = generatePassword();
+// password randomizer based on selected user criteria
+function generatePassword() {
+  var password = '';
+  var charSet = '';
+  if (lower) charSet += letters;
+  if (upper) charSet += upperLetters;
+  if (digitsOpt) charSet += digits;
+  if (specCharOpt) charSet += specChar;
+  
+  console.log(charSet);
+  
+  // for loop adapted from @MelvinAdekanye on Youtube
+  for (var i, i = 0; i < passLength; i++) {
+    password += charSet.charAt(Math.floor(Math.random() * charSet.length))
+  }
+  console.log(password);
+
+  
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
-}
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+};
 
 
 
 
-/** click event = begin prompt functions
- * PROMPT for character length
- *   if length does not fall between 8 and 128, add ALERT to remind user of criteria and restart function
- * CONFIRM for lowercase characters
- * CONFIRM for uppercase characters
- * CONFIRM for special characters
- * CONFIRM all criteria is correct
- *   if true, continue to password generator function, if false restart function
- */
-
-/** must build generator function
- * add lowercase array if lower === true
- * add uppercase array if lower === true
- * add numbers array if digits === true
- * add special characters if specChar === true
- * 
- * function generatePassword() {
- * var characters = function
- * for (var i, i=0; i < passLength; i++){
- *   password_string += characters.charAt(math.floor(math.random() * characters.lenght))}}
- */
 
